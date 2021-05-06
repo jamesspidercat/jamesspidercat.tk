beb5a2ff5a5b49f33ffed662801cf93d5590d24f
Sam <33501404+jamesspidercat@users.noreply.github.com>
2021-05-06 14:22:31 +1200

    Shannons logout idea

--- logout.php
+++ logout.php
@@ -6,22 +6,48 @@ $require_login = '1';
 $jsPaths = array('js/main.js','js/no_resubmit.js');
 require_once('page_top.php');
 ?>
-<form name="logout" method="post" class="col-12 m-5" action="<?php echo $_SERVER['PHP_SELF'];?>">
-        <h3>Are you sure you want to logout?</h3>
-        <div class="form-group">
-            <button type="submit" name="submit" class="btn btn-primary" value="logout">Logout</button>
-        </div>
-</form>
+<div class="container">
+<form name="logout" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+            <h3>Are you sure you want to logout?</h3>
+            <div class="row">
+                <button type="submit" name="yes" class="btn btn-primary col-1 m-1" value="logout">Yes Please</button>
+                <button type="submit" name="maybe" class="btn btn-primary col-1 m-1" value="unsure">I'm Not Sure</button>
+                <button type="submit" name="no" class="btn btn-primary col-1 m-1" value="no">No Thanks!</button>
+            </div>
+    </form>
+</div>
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    unset( $_SESSION["uid"] );
-    unset( $_SESSION["uname"] );
-    unset( $_SESSION["uperms"] );
+    $random = false;
+    if (isset($_POST['maybe'])) {
+        if (rand(0, 1)) {
+            $random = true;
+            $alert_text = "The Gods Have Decided: You have been logged out!";
+        }else{
+            $alert_text = 'The Gods Have Decided: You will remain logged in!';
+        }
+    }
+    if (isset($_POST['yes']) || $random == true) {
+        # Save-button was clicked
+        unset( $_SESSION["uid"] );
+        unset( $_SESSION["uname"] );
+        unset( $_SESSION["uperms"] );
+        if (isset($_POST['yes'])) {
+            $alert_text = 'Successfuly Logged out!';
+        }
+    
+    }
+    elseif (isset($_POST['no'])) {
+        $alert_text = 'Okay...';
+    }
     print '
-<script>
-    alert( "Successfuly Logged out!" );
-    location = "index.php";
-</script>';
+    <script>
+        alert( "'.$alert_text.'" );
+        location = "index.php";
+    </script>';
+
+
 }
 include ("page_bottom.php");
 ?>
