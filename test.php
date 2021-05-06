e4d04f9069911ecbaaa5ab90a2fa9b5d49824563
Sam <jamesspidercat@gmail.com>
2021-05-06 17:53:25 +1200

    Update test.php

--- test.php
+++ test.php
@@ -7,17 +7,25 @@ $jsPaths = array('js/main.js');
 require_once('page_top.php');
 ?>
 <div class="container" style="color: white;">
-    <div class="row">
-        <div class="col-8">
+    <div class="row justify-content-between"><!-- start between end center (if only 1 in middle)-->
+        <div class="col-4">
             Irure do cillum dolore adipisicing in. Eu excepteur duis commodo tempor aliqua aliqua commodo nostrud et ex ex esse. Elit Lorem incididunt occaecat non in velit reprehenderit proident. In deserunt quis consequat eiusmod ad exercitation commodo. Aute do cillum magna sunt. Ipsum ut incididunt reprehenderit dolore ad quis qui reprehenderit.
         </div>
         <div class="col-4">
-            <img src="imgs/book_stack.jpg">
+            <img src="imgs/book_stack.jpg" width="70%">
         </div>
     </div>
     <div class="row">
-        <div class="col-4">
-            Esse ea irure velit irure consectetur. Nostrud nulla sint deserunt dolore enim deserunt laborum commodo deserunt esse in nostrud. Cillum et qui ad sint dolor in duis sit non. Ea magna aliqua consequat do voluptate incididunt adipisicing anim ea. Incididunt nisi non voluptate laboris labore commodo laboris cillum esse cillum est.
+        <div class="col-8">
+            Et velit culpa cillum ad adipisicing Lorem est voluptate qui excepteur elit reprehenderit. Occaecat mollit irure culpa duis et. Duis proident dolore dolor proident esse. Ad anim tempor cillum cillum enim et eu. Nisi veniam anim ad ullamco in sint sint officia ullamco non minim. Esse eiusmod proident in deserunt. Voluptate minim ullamco ex cillum.
+
+Do sunt labore tempor aliqua enim et. Nulla culpa aute ipsum do. Esse aute culpa sunt culpa veniam est culpa veniam consectetur laborum eiusmod reprehenderit.
+
+Et mollit incididunt voluptate sit. Ex sint est aliqua excepteur incididunt dolor minim esse consectetur pariatur incididunt et. Magna qui fugiat excepteur dolor amet quis laborum irure pariatur. Sit nostrud sit in quis magna magna incididunt fugiat proident dolor commodo laboris.
+
+Minim aliqua nisi fugiat proident ea et. Aliqua laboris duis sint commodo laboris fugiat magna consectetur ut. Aliqua duis adipisicing enim mollit consequat id veniam. Dolore consectetur esse commodo in voluptate.
+
+Anim elit deserunt cillum mollit eiusmod magna est aliquip. Ad dolor duis aliquip cillum pariatur. Velit tempor dolor deserunt fugiat voluptate sunt sint incididunt ad. Velit culpa ad eu aute dolore occaecat anim dolor et veniam qui.
         </div>
     </div>
 </div>
