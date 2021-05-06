5146d2a9246b2268b485c843bb14cef90b67b6f3
Sam <33501404+jamesspidercat@users.noreply.github.com>
2021-05-06 15:10:13 +1200

    test page

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+//page_top.php var setup
+$page_title = "thispage | Sam's Books";
+$curPage  = "thispage";
+$require_login = '0';
+$jsPaths = array('js/main.js');
+require_once('page_top.php');
+?>
+<div class="container" style="color: white;">
+    <div class="row">
+        <div class="col-8">
+            Irure do cillum dolore adipisicing in. Eu excepteur duis commodo tempor aliqua aliqua commodo nostrud et ex ex esse. Elit Lorem incididunt occaecat non in velit reprehenderit proident. In deserunt quis consequat eiusmod ad exercitation commodo. Aute do cillum magna sunt. Ipsum ut incididunt reprehenderit dolore ad quis qui reprehenderit.
+        </div>
+        <div class="col-4">
+            <img src="imgs/book_stack.jpg">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            Esse ea irure velit irure consectetur. Nostrud nulla sint deserunt dolore enim deserunt laborum commodo deserunt esse in nostrud. Cillum et qui ad sint dolor in duis sit non. Ea magna aliqua consequat do voluptate incididunt adipisicing anim ea. Incididunt nisi non voluptate laboris labore commodo laboris cillum esse cillum est.
+        </div>
+    </div>
+</div>
+<?php
+include ("page_bottom.php");
+?>
