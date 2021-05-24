56037eb6e8b96bd69ade62c426255ccdedc2638a
Sam <33501404+jamesspidercat@users.noreply.github.com>
2021-05-24 13:08:11 +1200

    created form, still needs php sql input

---
+++ add_book.php
@@ -0,0 +1,106 @@
+<?php
+//page_top.php var setup
+$page_title = "Add Book | Sam's Books";
+$curPage  = "addbook";
+$require_login = '3';
+$jsPaths = array('js/main.js','js/no_resubmit.js');
+require_once('page_top.php');
+?>
+<script>
+$(function(){
+    var requiredCheckboxes = $('.formats :checkbox[required]');
+    requiredCheckboxes.change(function(){
+        if(requiredCheckboxes.is(':checked')) {
+            requiredCheckboxes.removeAttr('required');
+        } else {
+            requiredCheckboxes.attr('required', 'required');
+        }
+    });
+});
+$(function(){
+    var requiredCheckboxes = $('.ownedin :checkbox[required]');
+    requiredCheckboxes.change(function(){
+        if(requiredCheckboxes.is(':checked')) {
+            requiredCheckboxes.removeAttr('required');
+        } else {
+            requiredCheckboxes.attr('required', 'required');
+        }
+    });
+});
+function setThreeNumberDecimal(event) {
+    number = document.getElementById("series_number").value;
+    number = parseFloat(number).toFixed(3);
+    document.getElementById("series_number").value = number;
+}
+</script>
+<div class="row" style="margin-left: 10px; margin-right: 10px;">
+    <form name="add_book" class="col-12" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" validate>
+        <h3>Add book</h3>
+        <div class="form-group">
+            <label for="book_name" class="form-label">Title*</label> 
+            <input id="book_name" type="text" class="form-control" name="book_name" maxlength="255" required>
+        </div>
+        <div class="form-group">
+            <label for="author" class="form-label">Author*</label> 
+            <input id="author" type="text" class="form-control" name="author" maxlength="255" required>
+        </div>
+        <div class="form-group">
+            <label for="series_number" class="form-label">Series Number</label>
+            <input id="series_number" type="number" class="form-control" placeholder="0.000" name="series_number" max='9999999.999' min='-9999999.999' step="0.001"onchange="
+            setThreeNumberDecimal()">
+        </div>
+        <div class="form-group">
+            <label for="series_name" class="form-label">Series Name</label>
+            <input id="series_name" type="text" class="form-control" name="series_name" maxlength="255">
+        </div>
+        <div class="form-group">
+            <label for="book_type" class="form-label">Book Type*</label>
+            <select class="form-select" id="book_type" name="book_type" required>
+                <option selected disabled value="">Choose...</option>
+                <option value="Prose">Prose</option>
+                <option value="Comic">Comic</option>
+                <option value="Audio Drama">Audio Drama</option>
+                <option value="Other">Other</option>
+			</select>
+        </div>
+        <div class="form-group">
+            <label for="collected_in" class="form-label">Collected In</label>
+            <input id="collected_in" type="text" class="form-control" name="collected_in" maxlength="255">
+            <small class="form-text text-white">Enter id's of books this book is collected in, in a comma seperated list (i.e. 123,456,789)</small>
+        </div>
+        <div class="form-group formats">
+            <label class="form-label">Format Owned In*</label>
+            <br>
+            <input type="checkbox" id="physical" name="physical" value="physical" required>
+            <label for="physical" class="form-label">Physical</label>
+            <input type="checkbox" id="digital" name="digital" value="digital" required>
+            <label for="digital" class="form-label">Digital</label>
+            <input type="checkbox" id="audio" name="audio" value="audio" required>
+            <label for="audio" class="form-label">Audio</label>
+        </div>
+        <div class="form-group ownedin">
+            <label class="form-label">Location Owned*</label>
+            <br>
+            <?php
+            $owned = ['Paperback','Hardback','Floppy','Box_Set','Kindle','Comixology','Big_Finish','Humble_Bundle','Fanatical','Audible','Unbound','Other'];
+            foreach($owned as $i){
+                print '<input type="checkbox" id="'.$i.'" name="'.$i.'" value="'.$i.'"required>
+                <label for="'.$i.'" class="form-label" styles>'.str_replace('_',' ',ucfirst($i)).' </label>';
+            }
+            unset($i)
+            ?>
+        </div>
+        <div class="form-group">
+            <label for="notes" class="form-label">Notes</label>
+            <input id="notes" type="text" class="form-control" name="notes" maxlength="65000">
+        </div>
+        <br>
+        <div class="form-group">
+            <button type="submit" name="submit" class="btn btn-primary" value="submit">Add</button>
+        </div>
+        <p id="signup_attempt"></p>
+    </form>
+</div>
+<?php
+include ("page_bottom.php");
+?>
