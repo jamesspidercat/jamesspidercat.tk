e0449bcc080361ba2602e12f014817d98974c56e
Sam <33501404+jamesspidercat@users.noreply.github.com>
2021-05-26 11:14:08 +1200

    Create create_post.php

---
+++ create_post.php
@@ -0,0 +1,12 @@
+<?php
+//page_top.php var setup
+$page_title = "Create Post | Sam's Books";
+$curPage  = "create_post";
+$require_login = '3';
+$jsPaths = array('js/main.js');
+require_once('page_top.php');
+?>
+
+<?php
+include ("page_bottom.php");
+?>
