82ea428390cc750eaf301617913ff4a3cd71189d
Sam <33501404+jamesspidercat@users.noreply.github.com>
2021-06-03 15:04:31 +1200

    started work on creating the post
    ahhhhhhhhhh

--- create_post.php
+++ create_post.php
@@ -1,12 +1,43 @@
 <?php
 //page_top.php var setup
-$page_title = "Create Post | Sam's Books";
-$curPage  = "create_post";
-$require_login = '3';
+$page_title = "thispage | Sam's Books";
+$curPage  = "thispage";
+$require_login = '0';
 $jsPaths = array('js/main.js');
 require_once('page_top.php');
 ?>
-
+<html lang="en" >
+    <button onclick="new_box()">new box</button>
+<section class="container" id="list-holder">
+  
+  <div class="list-item">
+    <div class="item-content">
+      <span class="order">1</span> Alpha
+    </div>
+  </div>
+  
+  <div class="list-item">
+    <div class="item-content">
+      <span class="order">2</span> Bravo
+    </div>
+  </div>
+  
+  <div class="list-item">
+    <div class="item-content">
+      <span class="order">3</span> Charlie
+    </div>
+  </div>
+  
+  <div class="list-item">
+    <div class="item-content">
+      <span class="order">4</span> Delta
+    </div>
+  </div>
+  
+</section>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.4/TweenMax.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.4/utils/Draggable.min.js'></script>
+<script  src="js/create_post.js"></script>
 <?php
 include ("page_bottom.php");
 ?>
