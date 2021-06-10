811c1d37b9e3e44c500ee3294041d51698f08fcd
Sam <33501404+jamesspidercat@users.noreply.github.com>
2021-06-10 15:10:24 +1200

    did stuff

--- create_post.php
+++ create_post.php
@@ -7,36 +7,40 @@ $jsPaths = array('js/main.js','js/create_post.js');
 require_once('page_top.php');
 ?>
 <div class="container row">
-	<section class="col-8">
-		asdfghj
+	<section class="col-8" id="post-elements">
 	</section>
-	<aside class="col-4 seperator">
-
-			<button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown">Create Element</button>
-			<ul class="dropdown-menu">
-				<li><button class="dropdown-item" type="button" onclick="create_element('text')">Text</button></li>
-				<li><button class="dropdown-item" type="button" onclick="create_element('image')">Image</button></li>
-				<li><button class="dropdown-item" type="button" onclick="create_element('video')">Video</button></li>
-				<li><button class="dropdown-item" type="button" onclick="create_element('audio')">Audio</button></li>
-			</ul>
-			<br>
-			<button class="btn btn-primary" onclick="save()">
-				Save
-			</button>
-			<br>
-			<button class="btn btn-primary" onclick="preview()">
-				Preview & Save
-			</button>
-			<br>
-			<label for="visabilty">Visabilty:</label>
-			<select name="visabilty" id="visabilty" class="btn btn-primary">
-				<option value="0">All</option>
-				<option value="1">User</option>
-				<option value="2">Vip</option>
-				<option value="3">Mod</option>
-				<option value="4">Admin</option>
-			</select>
-			<hr>
+	<aside class="col-4 seperator text-white">
+		<h3>Post Options</h3>
+		<button class="btn btn-primary dropdown-toggle post-options" type="button" data-bs-toggle="dropdown">
+			Create Element
+		</button>
+		<ul class="dropdown-menu">
+			<li><button class="dropdown-item" type="button" onclick="create_element('text')">Text</button></li>
+			<li><button class="dropdown-item" type="button" onclick="create_element('image')">Image</button></li>
+			<li><button class="dropdown-item" type="button" onclick="create_element('video')">Video</button></li>
+			<li><button class="dropdown-item" type="button" onclick="create_element('audio')">Audio</button></li>
+		</ul>
+		<br>
+		<button class="btn btn-primary post-options" onclick="save()">
+			Save
+		</button>
+		<br>
+		<button class="btn btn-primary post-options" onclick="preview()">
+			Preview & Save
+		</button>
+		<br>
+		
+		<select name="visabilty" id="visabilty" class="btn btn-primary post-options">
+			<option value="0">All</option>
+			<option value="1">User</option>
+			<option value="2">Vip</option>
+			<option value="3">Mod</option>
+			<option value="4">Admin</option>
+		</select>
+		<label for="visabilty">Visabilty</label>
+		<hr>
+		<h3>Element Options</h3>
+		<div id="element-options"></div>
 	</aside>
 </div>
 <?php
--- js/create_post.js
+++ js/create_post.js
@@ -1,9 +1,60 @@
 function create_element(type){
-
+	var new_element = document.createElement("div");
+	document.getElementById("post-elements").appendChild(new_element);
+	if (type != 'text' && type != 'blank'){
+		new_element.setAttribute("data-file-width","100%")
+		new_element.setAttribute("data-file","")
+	}
+	new_element.setAttribute("data-width","3");
+	new_element.setAttribute("data-type",type);
+	new_element.setAttribute("data-text","");
+	new_element.setAttribute("data-align","start");
+	new_element.setAttribute("class","post-element");
+	new_element.setAttribute("onclick","edit_element(this)")
 }
 function save(){
 
 }
 function preview(){
 	save();
+}
+function moveChoiceTo(elem_choice, direction) {
+
+    var span = elem_choice.parentNode,
+        td = span.parentNode;
+
+    if (direction === -1 && span.previousElementSibling) {
+        td.insertBefore(span, span.previousElementSibling);
+    } else if (direction === 1 && span.nextElementSibling) {
+        td.insertBefore(span, span.nextElementSibling.nextElementSibling)
+    }
+}
+function edit_element(element){
+	console.log(element);
+	var element_options = document.getElementById("element-options");
+	switch (element.dataset.type){
+		case 'image':
+			accept = ".jpeg, .jpg, .gif, .png, .webp"
+			break;
+		case 'video':
+			accept = ".mp4, .webm"
+			break;
+		case 'audio':
+			accept = ".mp3, .wav"
+			break;
+		case 'text':
+			//text
+			//align
+		case 'blank':
+			//delete
+			break;
+	}
+	if (element == //audio/video/image){
+		//add file
+		var new_element = document.createElement("input");
+		element_options.appendChild(new_element);
+		new_element.setAttribute("type","file");
+		new_element.setAttribute("accept",accept);
+	}
+	//file width
 }
