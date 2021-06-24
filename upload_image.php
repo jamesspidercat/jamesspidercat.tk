06367d237b64b6a08117a19c491accddddbab337
Sam <33501404+jamesspidercat@users.noreply.github.com>
2021-06-24 14:38:12 +1200

    creates folder for image if it does not exist

--- upload_image.php
+++ upload_image.php
@@ -2,15 +2,18 @@
 
 if(isset($_FILES['file']['name'])){
 
-   /* Getting file name */
+   // Getting file name
    $filename = $_FILES['file']['name'];
 
-   /* Location */
+   // Location
    $location = "blog/".$_GET['id']."/".$filename;
+   if (!file_exists("blog/".$_GET['id'])) {
+      mkdir("blog/".$_GET['id'], 0777, true);
+  }
    $imageFileType = pathinfo($location,PATHINFO_EXTENSION);
    $imageFileType = strtolower($imageFileType);
-
-   /* Valid extensions */
+   
+   // Valid extensions
    $valid_extensions = array();
    switch ($_GET['type']){
       case "image":
@@ -22,11 +25,10 @@ if(isset($_FILES['file']['name'])){
       case "audio":
          $valid_extensions = array("mp3", "wav");
    }
-
    $response = 0;
-   /* Check file extension */
+   // Check file extension
    if(in_array(strtolower($imageFileType), $valid_extensions)) {
-      /* Upload file */
+      // Upload file
       if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
          $response = $location;
       }
